eb3ec7d67f1109fd3940970d70cf107e7c6dbebc
leekilhun <pba7189@gmail.com>
2022-09-25 21:51:55 +0900

    초기 init

---
+++ mfcVS2022_cli/mfc_cli/mfc_cli.cpp
@@ -0,0 +1,106 @@
+﻿
+// mfc_cli.cpp: 애플리케이션에 대한 클래스 동작을 정의합니다.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "mfc_cli.h"
+#include "mfc_cliDlg.h"
+
+
+
+// MFC 프로젝에서 윈도우 콘솔 출력 
+#ifdef _UNICODE
+#pragma comment(linker, "/entry:wWinMainCRTStartup /subsystem:console")
+#else
+#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
+#endif
+
+
+#include <stdio.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CmfccliApp
+
+BEGIN_MESSAGE_MAP(CmfccliApp, CWinApp)
+	ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
+END_MESSAGE_MAP()
+
+
+// CmfccliApp 생성
+
+CmfccliApp::CmfccliApp()
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+	// InitInstance에 모든 중요한 초기화 작업을 배치합니다.
+}
+
+
+// 유일한 CmfccliApp 개체입니다.
+
+CmfccliApp theApp;
+
+
+// CmfccliApp 초기화
+
+BOOL CmfccliApp::InitInstance()
+{
+	CWinApp::InitInstance();
+
+	if (!AfxSocketInit())
+	{
+		AfxMessageBox(IDP_SOCKETS_INIT_FAILED);
+		return FALSE;
+	}
+
+
+
+
+
+	FILE* fp = nullptr;
+	errno_t ret = fopen_s(&fp, "CON", "w");
+	if (ret == ERROR_SUCCESS)
+	{
+		fprintf_s(fp, "hellow file I/O \n");
+		fclose(fp);
+	}
+
+	printf("printf out \n");
+
+
+
+
+
+	CmfccliDlg dlg;
+	m_pMainWnd = &dlg;
+	INT_PTR nResponse = dlg.DoModal();
+	if (nResponse == IDOK)
+	{
+		// TODO: 여기에 [확인]을 클릭하여 대화 상자가 없어질 때 처리할
+		//  코드를 배치합니다.
+	}
+	else if (nResponse == IDCANCEL)
+	{
+		// TODO: 여기에 [취소]를 클릭하여 대화 상자가 없어질 때 처리할
+		//  코드를 배치합니다.
+	}
+	else if (nResponse == -1)
+	{
+		TRACE(traceAppMsg, 0, "경고: 대화 상자를 만들지 못했으므로 애플리케이션이 예기치 않게 종료됩니다.\n");
+		TRACE(traceAppMsg, 0, "경고: 대화 상자에서 MFC 컨트롤을 사용하는 경우 #define _AFX_NO_MFC_CONTROLS_IN_DIALOGS를 수행할 수 없습니다.\n");
+	}
+
+
+#if !defined(_AFXDLL) && !defined(_AFX_NO_MFC_CONTROLS_IN_DIALOGS)
+	ControlBarCleanUp();
+#endif
+
+	// 대화 상자가 닫혔으므로 응용 프로그램의 메시지 펌프를 시작하지 않고 응용 프로그램을 끝낼 수 있도록 FALSE를
+	// 반환합니다.
+	return FALSE;
+}
+
